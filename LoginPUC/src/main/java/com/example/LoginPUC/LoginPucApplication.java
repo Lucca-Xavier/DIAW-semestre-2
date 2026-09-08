3734799e8d202010cd8fe2bb13756a5fc2bb3280
Lucca Xavier <luccacxavier@gmail.com>
2026-09-08 19:56:15 -0300

    initial commit LoginPUC

---
+++ LoginPUC/src/main/java/com/example/LoginPUC/LoginPucApplication.java
@@ -0,0 +1,13 @@
+package com.example.LoginPUC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = {"com.example"})
+public class LoginPucApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginPucApplication.class, args);
+	}
+
+}
